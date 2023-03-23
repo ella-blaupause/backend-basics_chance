627c7296fb9eb5845f4df4e4516f61a8e4222419
Ella Kempel <ella.lernt@mail.de>
2023-03-23 13:56:33 +0100

    add solution

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+const name = chance.string({ pool: "elkmp" });
+const age = chance.integer({ min: 2, max: 99 });
+const profession = chance.string({ pool: "abcdefghi" });
+
+console.log(name);
+export const server = createServer((request, response) => {
+  response.end(
+    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+  );
+});
